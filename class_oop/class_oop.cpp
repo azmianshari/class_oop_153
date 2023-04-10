a3c9284c805280a206d5273626e35bc912808ada
azmianshari <azmianshari123@gmail.com>
2023-04-10 14:44:55 +0700

    Add project files.

---
+++ class_oop/class_oop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public :
+	int nim;
+	string nama;
+	void tampil() {
+		cout << "Nim = " << nim;
+		cout << "Nama = " << nama;
+	}
+};
+
+class Matakuliah {
+private :
+	int sks;
+	string kode;
+	string namaMk;
+public :
+
+};
